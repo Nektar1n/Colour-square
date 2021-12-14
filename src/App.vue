e26bf6b901c3fbbbef29978c0b8786fe66b24362
Nikita <vysokij96@bk.ru>
2021-12-14 15:30:32 +0300

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <div class="itemsLists">
+        <div class="allTitle"><div class="showBtn" @click="showItem1()" id="show1">❯</div><div class="titles"></div>
+        <input type="checkbox" class="customCheckbox" v-model="hasChekedList1">List 1</div><div class="listTitle">List 1</div>
+        <items v-show="hasShowItem1" :hasChekedList1="hasChekedList1"></items>
+        <div class="allTitle"><div class="showBtn" @click="showItem2()" id="show2">❯</div><div class="titles"></div>
+          <input type="checkbox" class="customCheckbox" v-model="hasChekedList2">List 2</div><div class="listTitle">List 2</div>
+        <items v-show="hasShowItem2" :hasChekedList2="hasChekedList2"/>
+        <div class="allTitle"><div class="showBtn" @click="showItem3()" id="show3">❯</div><div class="titles"></div>
+          <input type="checkbox" class="customCheckbox" v-model="hasChekedList3">List 3</div><div class="listTitle">List 3</div>
+        <items v-show="hasShowItem3" :hasChekedList3="hasChekedList3"/>
+        <div class="allTitle"><div class="showBtn" @click="showItem4()" id="show4">❯</div><div class="titles"></div>
+          <input type="checkbox" class="customCheckbox" v-model="hasChekedList4">List 4</div><div class="listTitle">List 4</div>
+        <items v-show="hasShowItem4" :hasChekedList4="hasChekedList4"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import items from "@/components/items";
+
+export default {
+  name: 'App',
+  components: {
+    items
+  },
+  data(){
+    return{
+      itemList:[1,2,3,4],
+      hasChekedList1:false,
+      hasChekedList2:false,
+      hasChekedList3:false,
+      hasChekedList4:false,
+      hasShowItem1:true,
+      hasShowItem2:true,
+      hasShowItem3:true,
+      hasShowItem4:true
+    }
+  },methods:{
+    showItem1(){
+      if (this.hasShowItem1){
+        this.hasShowItem1=false
+        document.getElementById('show1').style='transform: rotate(360deg)'
+      }else {this.hasShowItem1=true
+      document.getElementById('show1').style='transform: rotate(90deg)'}
+    },
+    showItem2(){
+      if (this.hasShowItem2){
+        this.hasShowItem2=false
+        document.getElementById('show2').style='transform: rotate(360deg)'
+      }else {this.hasShowItem2=true
+        document.getElementById('show2').style='transform: rotate(90deg)'}
+    },
+    showItem3(){
+      if (this.hasShowItem3){
+        this.hasShowItem3=false
+        document.getElementById('show3').style='transform: rotate(360deg)'
+      }else {this.hasShowItem3=true
+        document.getElementById('show3').style='transform: rotate(90deg)'}
+    },showItem4(){
+      if (this.hasShowItem4){
+        this.hasShowItem4=false
+        document.getElementById('show4').style='transform: rotate(360deg)'
+      }else {this.hasShowItem4=true
+        document.getElementById('show4').style='transform: rotate(90deg)'}
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  margin: 10%;
+}
+
+.listTitle{
+  position: relative;
+  left: 55%;
+  bottom: 20px;
+  font-size: 20px;
+}
+.titles{
+  position: relative;
+  top: 20px;
+}
+input[type='checkbox']{
+  transform: scale(1.5);
+}
+.allTitle{
+  display: flex;
+  width: 300px;
+  font-size: 20px;
+}
+.showBtn{
+  margin-right: 10px;
+}
+.customCheckbox{
+  margin-right: 15px;
+  margin-top: 5px;
+}
+.itemsLists{
+  border: solid 2px gray;
+  padding: 10px;
+}
+.showBtn{
+  cursor: pointer;
+  transform: rotate(90deg);
+}
+
+</style>
+
---
+++ src/components/items.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="wrapper">
+    <div class="items">
+      <dl>
+        <dt>
+          <input type="checkbox" v-model="checkedList1"><label> item 1</label><input type="number" max="1000" min="0" v-model="itemNumber1" placeholder="number of squares"><input type="color" class="colourSquare" v-model="itemColour1">
+        </dt>
+        <dt>
+          <input type="checkbox" v-model="checkedList2"><label> item 2</label><input type="number" max="1000" min="0" v-model="itemNumber2" placeholder="number of squares"><input type="color" class="colourSquare" v-model="itemColour2">
+        </dt>
+        <dt>
+          <input type="checkbox" v-model="checkedList3"><label> item 3</label><input type="number" max="1000" min="0" v-model="itemNumber3" placeholder="number of squares"><input type="color" class="colourSquare" v-model="itemColour3">
+        </dt>
+        <dt>
+          <input type="checkbox" v-model="checkedList4"><label> item 4</label><input type="number" max="1000" min="0" v-model="itemNumber4" placeholder="number of squares"><input type="color" class="colourSquare" v-model="itemColour4">
+        </dt>
+      </dl>
+    </div>
+    <div class="showItems">
+      <div class="list"></div>
+      <span v-show="checkedList1" v-for="item in Number(itemNumber1)" :key="item" class="itemList"><div class="myList" :style="{background:itemColour1}">{{item}}</div></span><br>
+      <span v-show="checkedList2" v-for="item in Number(itemNumber2)" :key="item" class="itemList"><div class="myList" :style="{background:itemColour2}">{{item}}</div></span><br>
+      <span v-show="checkedList3" v-for="item in Number(itemNumber3)" :key="item" class="itemList"><div class="myList" :style="{background:itemColour3}">{{item}}</div></span><br>
+      <span v-show="checkedList4" v-for="item in Number(itemNumber4)" :key="item" class="itemList"><div class="myList" :style="{background:itemColour4}">{{item}}</div></span><br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'items',
+  data(){
+    return{
+      itemNumber1:null,
+      itemNumber2:null,
+      itemNumber3:null,
+      itemNumber4:null,
+      checkedList1:false,
+      checkedList2:false,
+      checkedList3:false,
+      checkedList4:false,
+      itemColour1:'',
+      itemColour2:'',
+      itemColour3:'',
+      itemColour4:'',
+      numberItems:[1,2,3,4]
+    }
+  },
+  props:{
+    hasChekedList1:{
+      type:Boolean
+    },hasChekedList2:{
+      type:Boolean
+    },hasChekedList3:{
+      type:Boolean
+    },hasChekedList4:{
+      type:Boolean
+    }
+  },watch:{
+    hasChekedList1:function (){
+      if(this.hasChekedList1===true){
+      this.checkedList1=true
+      this.checkedList2=true
+      this.checkedList3=true
+      this.checkedList4=true}
+      if(this.hasChekedList1===false){
+        this.checkedList1=false
+        this.checkedList2=false
+        this.checkedList3=false
+        this.checkedList4=false
+      }
+    },hasChekedList2:function (){
+      if(this.hasChekedList2===true){
+        this.checkedList1=true
+        this.checkedList2=true
+        this.checkedList3=true
+        this.checkedList4=true}
+      if(this.hasChekedList2===false){
+        this.checkedList1=false
+        this.checkedList2=false
+        this.checkedList3=false
+        this.checkedList4=false
+      }
+    },hasChekedList3:function (){
+      if(this.hasChekedList3===true){
+        this.checkedList1=true
+        this.checkedList2=true
+        this.checkedList3=true
+        this.checkedList4=true}
+      if(this.hasChekedList3===false){
+        this.checkedList1=false
+        this.checkedList2=false
+        this.checkedList3=false
+        this.checkedList4=false
+      }
+    },hasChekedList4:function (){
+      if(this.hasChekedList4===true){
+        this.checkedList1=true
+        this.checkedList2=true
+        this.checkedList3=true
+        this.checkedList4=true}
+      if(this.hasChekedList4===false){
+        this.checkedList1=false
+        this.checkedList2=false
+        this.checkedList3=false
+        this.checkedList4=false
+      }
+    },
+  },
+  mounted() {
+    const inputs = document.querySelectorAll('input[type=number]')
+    Array.from(inputs).forEach(input => {
+      const min = +input.min;
+      const max = +input.max;
+
+      input.addEventListener('input', () => {
+        const value = +input.value;
+        if (value > max) {
+          input.value = max
+        } else if (value < min) {
+          input.value = min
+        }
+      })
+      })
+  }
+}
+</script>
+
+<style scoped>
+.wrapper{
+  display: flex;
+}
+.items{
+  width: 50%;
+  border: solid 2px gray;
+  margin-left: 100px;
+}
+.showItems{
+  width: 50%;
+  border: 2px solid olivedrab;
+  margin-left: 15px;
+  /*margin-top: 15px;*/
+  height: 100%;
+  padding-top: 10px;
+}
+.myList{
+  display: inline-block;
+  border: 2px solid gray;
+  margin-right: 2px;
+  font-size: 0;
+  width: 10px;
+  height: 10px;
+  background-color: black;
+}
+.itemList{
+}
+input[type='checkbox']{
+  transform: scale(1.3);
+}
+.colourSquare{
+  border:none;
+  width: 30px;
+  height: 30px;
+  background:none;
+  margin-left: 5px;
+  transition: all .5s ease;
+
+}
+.colourSquare:hover{
+  background: deepskyblue;
+  border-radius: 20px;
+}
+input[type='number']{
+  height: 30px;
+  font-size: 20px;
+  width: 200px;
+}
+@media (max-width: 600px) {
+  input[type='number']{
+    height: 20px;
+    font-size: 10px;
+    width: 20px;
+  }
+  .colourSquare{
+    width: 20px;
+    height: 20px;
+  }
+}
+</style>
